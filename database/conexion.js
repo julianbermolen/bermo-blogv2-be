5bb95400fa8b3942eed9202696a6f4be53f5be98
julianbermolen <julianbermolen@gmail.com>
2022-05-26 19:52:36 -0300

    db

--- database/conexion.js
+++ database/conexion.js
@@ -1,18 +1,18 @@
 const mysql = require('mysql');
 
-let conexion = {
+/*let conexion = {
     host: "localhost",
     user: "root",
     password:"",
     database:"bermo_blog"
-}
-/*let conexion = {
-    connectionLimit:3,
-    host: "portal.wi-sim.com.ar",
-    user:"Y3SJL2",
-    password:"LAISNLSB4VQLX8Z",
-    database:"Mikrowisp6"
 }*/
+let conexion = {
+    connectionLimit:3,
+    host: "julianbermolen.com",
+    user:"u130197610_julianbermolen",
+    password:"Julian123!",
+    database:"u130197610_julianbermolen"
+}
 
 var con = mysql.createPool(conexion);
 
